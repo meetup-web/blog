a5d2b40c6cb38565b7463f8c865810b86463dcb0
Nxtasunstrike <unixalle765@mail.ru>
2025-04-02 19:52:32 +0300

    feat(remove unused file config in infra)

--- src/blog/infrastructure/persistence/config.py
+++
@@ -1,6 +0,0 @@
-from importlib.resources import files
-
-
-def get_alembic_config() -> str:
-    resource = files("blog.infrastructure.persistence.alembic")
-    return resource.joinpath("alembic.ini").read_text("utf-8")
